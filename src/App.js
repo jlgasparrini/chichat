b149839ecd83e9655dafb691eaff9ebc71c8f82b
Leonel Gasparrini <leonel@plum.com.ar>
2020-06-18 16:34:17 -0300

    Add material-ui and update landing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import logo from './logo.png';
 import './App.css';
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
+
+        <h1 style={{ fontFamily: 'cursive' }}>
           CHICHAT
         </h1>
-        <p>
+
+        <h2>
           WHERE BULLYING HAPPENS
+        </h2>
+        <p style={{ fontFamily: 'italic' }}>
+          I'm joking, just to talk with your friends! You know...
+          Don't be a disrespectful motherfuc***.
         </p>
+
+        <Button variant="contained" size="large">
+          Touch to joke with your friends
+        </Button>
       </header>
     </div>
   );
